09c15408af6b00a5ab6f72c93e64fa32d168e36e
ashokkumariitc <ashok.kumar.iitc@gmail.com>
2019-02-05 12:18:42 -0600

    Comment Updated

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,12 +3,30 @@ import '../App.css';
 import First from './First'
 
 class Home extends Component {
+
+  constructor(props){
+    super(props);
+    this.goSecond = this.goSecond.bind(this);
+    this.goThird = this.goThird.bind(this);
+  }
+
+  goSecond(){
+    this.props.history.push('/second');
+  }
+
+  goThird(){
+    this.props.history.push('/third');
+  }
+
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           Connected React Router
         </header>
+        <button onClick={this.goSecond}>Go To Second</button>
+        <button onClick={this.goThird} >Go To Third</button>
         <div  className="app-body">
           <First />
         </div>
